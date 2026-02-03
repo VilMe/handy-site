05a502c83b8ea0d826efe5c227637e02ba3e6e70
VilMe <vilma.medrano00@gmail.com>
2026-02-03 15:32:11 -0600

    refactor code to remove redundent code

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            GO Handy Services - For all your home maintenance and handyman needs
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="./contact.php">Contact Us</a>
+        </div>
+        <header>
+            <h1>GO Handy Services</h1>
+                <img src="images/go-handy-no-bckgrnd.png" alt="GO Handy Services logo"/>
+        </header>
--- index.php
+++ index.php
@@ -1,38 +1,20 @@
 <?php 
-$navbarLinks = [ "Home", "Portfolio", "Contact Us"];
+$navbarLinks = [ "Home", "Completed Projects", "Contact Us"];
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            GO Handy Services - For all your home maintenance and handyman needs
-        </title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
-            <a href="./contact.php">Contact Us</a>
-        </div>
-        <header>
-            <h1>GO Handy Services</h1>
-                <img src="images/go-handy-no-bckgrnd.png" alt="GO Handy Services logo"/>
-        </header>
+
 
         <main>
             <article>
-                <h2> Greg Ocano</h2>
+                <h2>Greg Ocano</h2>
                 <h3>Owner, Small Job Specialist</h3>
-                    <ul>
-                        <li>Mantain</li>
-                        <li>Install</li>
-                        <li>Repair</li>
-                    </ul>
+
+                  <p>Mantain</p>
+                  <p>Install</p>
+                  <p>Repair</p>
                 
-                <h3><a href="./portfolio.php" title="Check out some of GregOs work">Past Jobs</a></h3>
+                <h3><a href="./portfolio.php" title="Check out some of GregOs work">Completed Projects</a></h3>
                 <h3>Contact Us</h3>
+                <p id="phone-number">Call <a href="tel:7078342295">(707)834-2295</a></p>  
             </article>
         </main>
     </body>
